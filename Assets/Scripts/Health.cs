bd9a256b1ce58ff6323296f908e3cbba87e59615
Fink <Finkjuhl@gmail.com>
2018-03-13 14:11:47 +0100

    todays work

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -2,13 +2,15 @@
 using System.Collections.Generic;
 using UnityStandardAssets.ImageEffects;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Health : MonoBehaviour
 {
-    public const int maxHealth = 100;
-    public int health = maxHealth;
+    public const float maxHealth = 100;
+    public float health = 100;
 
     public GameObject playerCamera;
+    public GameObject sliderUI;
 
     public bool healthDamage = true;
 
@@ -18,6 +20,7 @@ public class Health : MonoBehaviour
     private void Start()
     {
         timer = Time.time;
+        health = maxHealth;
     }
 
     public void TakeDamage(int amount)
@@ -25,6 +28,9 @@ public class Health : MonoBehaviour
         if (healthDamage)
         {
             health -= amount;
+
+            //access the slider
+            sliderUI.GetComponent<Slider>().value = health;
 
             if (health <= 0)
             {
@@ -53,13 +59,12 @@ public class Health : MonoBehaviour
         if (collision.gameObject.tag == "Enemy")
         {
             TakeDamage(10);
+            playerCamera.GetComponent<VignetteAndChromaticAberration>().chromaticAberration = ((maxHealth - health) / 100)*45;
+            playerCamera.GetComponent<VignetteAndChromaticAberration>().intensity = ((maxHealth - health) / 100.0f) * 0.6f;
+            playerCamera.GetComponent<VignetteAndChromaticAberration>().blur = ((maxHealth - health) / 100.0f) * 0.5f;
 
-            if (health == 90)
-            {
-                playerCamera.GetComponent<VignetteAndChromaticAberration>().chromaticAberration += 0.45f;
-                playerCamera.GetComponent<VignetteAndChromaticAberration>().intensity += 0.006f;
-                playerCamera.GetComponent<VignetteAndChromaticAberration>().blur += 0.005f;
-            }
+            playerCamera.GetComponent<Fisheye>().strengthX = ((maxHealth - health) / 100.0f) * 0.5f;
+            playerCamera.GetComponent<Fisheye>().strengthY = ((maxHealth - health) / 100.0f) * 0.5f;
 
             if (health == 0)
             {
--- Assets/Scripts/Movements.cs
+++ Assets/Scripts/Movements.cs
@@ -24,52 +24,10 @@ public class Movements : MonoBehaviour
         //this is cleaner:
         if (Vector3.Distance(transform.position, player.transform.position) < distanceOffset)
         {
-            //do something, but keep in mind the Y value is also considered in distanceOffset
-            //in which case you maybe use local copies of positions above, setting Y to zero
-        }
-
-        if (gameObject.transform.position.x - player.transform.position.x < distanceOffset
-         && gameObject.transform.position.z - player.transform.position.z < distanceOffset)       
-        {
             GetComponent<NavMeshAgent>().enabled = true;
             
             anim.SetFloat("left", Mathf.Sin(Time.time));
             nav.destination = player.transform.position;
         }
     }
-
-   /* void OnTriggerEnter(Collider other)
-    {
-        if (other.tag == "Player")
-        {
-            GetComponent<NavMeshAgent>().enabled = true;
-           // player = GameObject.FindGameObjectWithTag("Player");
-            anim.SetFloat("left", Mathf.Sin(Time.time));
-            Debug.Log(other.tag);
-        }
-    }
-
-    private void OnTriggerStay(Collider other)
-    {
-        if (gameObject.GetComponent<NavMeshAgent>().enabled == true)
-        {
-            nav.destination = player.transform.position;
-            Debug.Log("Staying");
-        }
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        gameObject.GetComponent<NavMeshAgent>().enabled = false;
-        Debug.Log("exit");
-    }
-
-    /*void OnTriggerEnter(Collider other)           //make this it's own perhaps
-    {
-        if(other.tag == "Player")
-        {
-            animateCombat = true;
-            anim.SetFloat("forward", 0.5f);
-        }
-    }*/
 }
